f4de7bb8d5563168d88e6b44ed6d42867b5457b2
mohammad derakhshan <hewillc0me@ymail.com>
2020-08-10 19:55:52 +0430

    completing signal detail activity

--- app/app/src/main/java/com/kharazmic/app/Address.kt
+++ app/app/src/main/java/com/kharazmic/app/Address.kt
@@ -17,10 +17,9 @@ class Address {
     val UserInfoAPI = "https://run.mocky.io/v3/8763b1c1-2bec-4b35-badc-7d9adc753df7"
 
 
-
     val TicketAPI = "https://run.mocky.io/v3/082ecf40-a6f3-44ef-a9f3-1568d2f299a8"
 
-
+    val signalDetailAPI = "https://run.mocky.io/v3/fb70bab7-217b-4e3f-a7b8-8d2955834678"
 
 
     fun NewsAPI(keyword: String, category: String, page: Int): String =
--- app/app/src/main/java/com/kharazmic/app/main/profile/signals/SignalDetailActivity.kt
+++ app/app/src/main/java/com/kharazmic/app/main/profile/signals/SignalDetailActivity.kt
@@ -2,29 +2,46 @@ package com.kharazmic.app.main.profile.signals
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 import com.github.florent37.expansionpanel.viewgroup.ExpansionLayoutCollection
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.utils.ColorTemplate
+import com.kharazmic.app.Address
+import com.kharazmic.app.Arrange
 import com.kharazmic.app.R
 import com.kharazmic.app.database.MyDatabase
 import com.kharazmic.app.databinding.ActivitySignalDetailBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
 
 
 class SignalDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignalDetailBinding
     private lateinit var database: MyDatabase
+    private lateinit var id: String
+
+    private val scope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_signal_detail)
         database = MyDatabase.getInstance(this)
         fetchDataFromDatabase()
+        fetchDataFromServer()
         ExpansionLayoutCollection().apply {
             add(binding.stockLayout)
             add(binding.descriptionLayout)
@@ -35,37 +52,21 @@ class SignalDetailActivity : AppCompatActivity() {
         }
 
 
-        val lineEntry = ArrayList<Entry>()
-        for (i in 2..10)
-            lineEntry.add(Entry(i.toFloat(), i - 2))
-
-        lineEntry.add(Entry(5F, 9))
-        lineEntry.add(Entry(10F, 10))
-        lineEntry.add(Entry(1F, 11))
-        lineEntry.add(Entry(6F, 12))
-
-
-        val lineLabels = ArrayList<String>()
-        for (i in 0..13)
-            lineLabels.add(i.toString())
-
-
-        val lineDataSet = LineDataSet(lineEntry, "").apply {
-            color = ContextCompat.getColor(baseContext, R.color.colorAccent)
-            setDrawCircles(false)
-            lineWidth = 3F
-            setDrawValues(false)
+        binding.refresh.setOnRefreshListener {
+            fetchDataFromServer()
         }
 
 
-        val lineData = LineData(lineLabels, lineDataSet)
-        binding.lineChart.data = lineData
-        binding.lineChart.animateXY(2000, 2000)
-
-
-
-        binding.lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-        binding.lineChart.axisRight.isEnabled = false
+        binding.lineChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            gridColor = ContextCompat.getColor(baseContext, R.color.light_gray)
+        }
+        binding.lineChart.axisRight.apply {
+            gridColor = ContextCompat.getColor(baseContext, R.color.light_gray)
+        }
+        binding.lineChart.axisLeft.isEnabled = false
+        binding.lineChart.setDescription("")
+        binding.lineChart.legend.isEnabled = false
 
 
     }
@@ -74,6 +75,7 @@ class SignalDetailActivity : AppCompatActivity() {
     private fun fetchDataFromDatabase() {
         intent.getStringExtra("category")?.let { category ->
             intent.getStringExtra("id")?.let { id ->
+                this.id = id
                 database.signalDAO.getStockInfo(category = category, id = id).observe(this,
                     Observer { stock ->
                         binding.title.text = stock.title
@@ -89,6 +91,72 @@ class SignalDetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun fetchDataFromServer() {
+        binding.refresh.isRefreshing = true
+        val info = JSONObject()
+        info.put("id", id)
+        val request = JsonObjectRequest(
+            Request.Method.POST,
+            Address().signalDetailAPI,
+            info,
+            Response.Listener {
+                scope.launch {
+
+                    withContext(Dispatchers.Main, block = {
+
+                        it?.let { data ->
+                            val profit = data.getInt("profit")
+                            val text =
+                                "این سهم تا این لحظه ${Arrange().persianConverter(profit.toString())} سود داده است. "
+
+                            binding.realTimeProfit.text = text
+
+                            val percentage = Arrange().persianConverter(profit.toString()) + "%"
+                            binding.percentage.text = percentage
+
+                            binding.profit.progress = profit.toFloat()
+                            if (profit > 0)
+                                binding.profit.progressBarColor =
+                                    ContextCompat.getColor(this@SignalDetailActivity, R.color.green)
+                            else
+                                binding.profit.progressBarColor =
+                                    ContextCompat.getColor(this@SignalDetailActivity, R.color.red)
+
+
+                            val lineEntry = ArrayList<Entry>()
+                            val lineLabels = ArrayList<String>()
+
+                            data.getJSONArray("chart").let { chart ->
+                                for (i in 0 until chart.length()) {
+                                    val point = chart.getJSONObject(i)
+                                    lineEntry.add(Entry(point.getInt("y").toFloat(), i))
+                                    lineLabels.add(Arrange().persianConverter(point.getString("x")))
+                                }
+                                val lineDataSet = LineDataSet(lineEntry, "").apply {
+                                    color = ContextCompat.getColor(baseContext, R.color.colorAccent)
+                                    setDrawCircles(false)
+                                    lineWidth = 2F
+                                    setDrawValues(false)
+                                }
+                                val lineData = LineData(lineLabels, lineDataSet)
+                                binding.lineChart.data = lineData
+                                binding.lineChart.animateXY(2000, 2000)
+
+                                binding.refresh.isRefreshing = false
+                            }
+                        }
+                    })
+                }
+            },
+            Response.ErrorListener {
+                binding.refresh.isRefreshing = false
+                binding.realTimeProfit.text = "خطا در بارگذاری اطلاعات!"
+                Log.i("Log", "Error in SignalDetailActivity $it")
+
+            })
+        val queue = Volley.newRequestQueue(this)
+        queue.add(request)
+    }
 
     override fun onBackPressed() {
         super.onBackPressed()
